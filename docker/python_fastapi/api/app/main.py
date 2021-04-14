b15b2f367304c3c0c008089f4f76edcca0edd8c8
Hiroaki Tagawa <tagawa0525@gmail.com>
2021-04-15 01:54:00 +0900

    fix CORS setting

--- docker/python_fastapi/api/app/main.py
+++ docker/python_fastapi/api/app/main.py
@@ -7,12 +7,14 @@ from starlette.requests import Request
 app = FastAPI()
 
 origins = [
+    # "http://192.168.1.*:3000"
     "http://localhost:3000"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- docker/python_fastapi/web/app/src/App.tsx
+++ docker/python_fastapi/web/app/src/App.tsx
@@ -13,7 +13,8 @@ class App extends React.Component<{}, { users: any[] }> {
   get axios() {
     const axiosBase = require('axios');
     return axiosBase.create({
-      baseURL: "http://localhost:8000",
+      baseURL: "http://192.168.1.108:8000",
+      // baseURL: "http://localhost:8000",
       headers: {
         'Content-Type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest'
